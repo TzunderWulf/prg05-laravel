9a3cb9383b941e2ea0e13c493acb652015633be4
TzunderWulf <svandenbos@tzunder.nl>
2021-09-28 10:42:07 +0200

    Added extra columns for role, amount of favorites and username in users table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role')->default('5');
+            $table->integer('amount_favorites')->default('0');
             $table->rememberToken();
-            $table->boolean('admin')->default('0');
             $table->timestamps();
         });
     }
