e02a496a7149048c136cfbba8b270a4e95f98a2d
TzunderWulf <svandenbos@tzunder.nl>
2021-09-29 22:13:58 +0200

    Function that gets data and pushes it through to the view, in the view it shows the data.

--- app/Http/Controllers/GenshinCharacterController.php
+++ app/Http/Controllers/GenshinCharacterController.php
@@ -2,32 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Character;
 use Illuminate\Http\Request;
 
 class GenshinCharacterController extends Controller
 {
-    public function returnCharacters()
+    public function index()
     {
-        // character list
-        $characters = [
-            [
-                'name' => 'Kaeya',
-                'element' => 'Cryo',
-                'city' => 'Mondstadt'
-            ],
-            [
-                'name' => 'Diluc',
-                'element' => 'Pyro',
-                'city' => 'Mondstadt'
-            ],
-            [
-                'name' => 'Jean',
-                'element' => 'Anemo',
-                'city' => 'Mondstadt'
-            ],
-        ];
+        $characters = Character::where('active', '1')->get();
 
-        // returns the view with list
         return view('genshin-characters', compact('characters'));
     }
 }
--- resources/views/genshin-characters.blade.php
+++ resources/views/genshin-characters.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.main')
 
 @section('content')
-
+    <div class="row row-cols-3 mx-5">
+        @foreach($characters as $character)
+            <div class="col">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h1 class="h3">{{ $character['first_name'] }} {{ $character['last_name'] }}</h1>
+                    </div>
+                    <div class="card-body">
+                        <p class="fw-bold">{{ \Illuminate\Support\Str::limit($character['description'], 130) }} </p>
+                        <a href="/character={{ $character['id'] }}" class="btn btn-outline-primary">Read more...</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
