608b9b798b914d346a288ee792e660b034b065b8
TzunderWulf <svandenbos@tzunder.nl>
2021-09-29 21:37:57 +0200

    Extended the Main layout to Welcome, Characters and About pages.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,8 +13,8 @@
         <nav class="navbar navbar-expand-md navbar-light pt-3 pb-3 mb-5 shadow">
             <div class="container-xxl">
                 <!-- navbar brand/title -->
-                <a href="" class="navbar-brand">
-                    <span class="text-primary fw-bold">{{ config('app.name', 'Genshin Wiki') }}</span>
+                <a href="{{ url('/') }}" class="navbar-brand">
+                    <span class="text-primary fw-bold">Genshin Wiki</span>
                 </a>
 
                 <!-- toggle button -->
@@ -63,5 +63,8 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </body>
 </html>
