77bca8f6f0f9ae692a0c2ab36a0163267230a38a
TzunderWulf <svandenbos@tzunder.nl>
2021-09-30 13:04:23 +0200

    Matched styling of the About page to the rest of the pages.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-10 rounded-border border border-2 m-2 pb-3">
-                <h1>{{$title}}</h1>
-                <p>{{$aboutWebsiteText}}</p>
-                <p>{{$infoAboutWebsite}}</p>
-            </div>
+    <div class="mx-5 row">
+        <div class="col-8 border m-2 p-3 ">
+            <h1>{{$title}}</h1>
+            <p>{{$aboutWebsiteText}}</p>
+            <p>{{$infoAboutWebsite}}</p>
         </div>
     </div>
 @endsection
