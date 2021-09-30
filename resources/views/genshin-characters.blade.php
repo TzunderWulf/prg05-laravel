715269a3015833416ad7860e6087a0f7e6faea64
TzunderWulf <svandenbos@tzunder.nl>
2021-09-30 14:24:50 +0200

    Added the right link on the "Read more..." button.

--- resources/views/genshin-characters.blade.php
+++ resources/views/genshin-characters.blade.php
@@ -1,19 +1,41 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="row row-cols-3 mx-5">
-        @foreach($characters as $character)
-            <div class="col">
-                <div class="card">
-                    <div class="card-header text-center">
-                        <h1 class="h3">{{ $character['first_name'] }} {{ $character['last_name'] }}</h1>
-                    </div>
-                    <div class="card-body">
-                        <p class="fw-bold">{{ \Illuminate\Support\Str::limit($character['description'], 130) }} </p>
-                        <a href="/character={{ $character['id'] }}" class="btn btn-outline-primary">Read more...</a>
+    <div class="mx-5">
+{{--        <div class="mb-3">--}}
+{{--            <h1>All characters in Genshin Impact</h1>--}}
+{{--            <h2 class="h5">Current amount on the wiki: {{count($characters) }}</h2>--}}
+{{--            <form method="post">--}}
+{{--                <div class="input-group">--}}
+{{--                    <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>--}}
+{{--                    <input type="text" class="form-control" placeholder="Search..." aria-label="Search term"--}}
+{{--                           aria-describedby="basic-addon1">>--}}
+{{--                </div>--}}
+{{--                <input type="submit" class="btn btn-primary" value="Search">--}}
+{{--            </form>--}}
+{{--        </div>--}}
+        <div class="row row-cols-3">
+            @foreach($characters as $character)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <h1 class="h3">{{ $character->first_name }} {{ $character->last_name }}</h1>
+                        </div>
+                        <div class="card-body">
+                            <p class="fw-bold">{{ \Illuminate\Support\Str::limit($character->description, 130) }} </p>
+                            <a href="/characters/{{ $character->id }}" class="btn btn-outline-primary mb-3">
+                                Read more...
+                            </a>
+{{--                            @if(auth::check())--}}
+{{--                                <form id="favorite" method="post" action="/characters/{{ $character->id }}">--}}
+{{--                                    <input type="hidden" name="favorite" value="1">--}}
+{{--                                    <input type="submit" class="btn btn-outline-success" value="Favorite">--}}
+{{--                                </form>--}}
+{{--                            @endif--}}
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
