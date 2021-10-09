6c95dde006083182f435afd493865d36e961fa71
TzunderWulf <svandenbos@tzunder.nl>
2021-10-09 13:57:56 +0200

    Made migration for 'characters table', added all neccessary columns.

---
+++ database/migrations/2021_10_09_115551_create_characters_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('title');
+            $table->longText('description');
+            $table->string('region');
+            $table->string('element');
+            $table->string('birthday');
+            $table->string('icon');
+            $table->string('portrait');
+            $table->foreignId('created_by');
+            $table->boolean('active')->default('1');
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
