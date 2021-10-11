7274029b2dcc568bd2fa1ce1a4f2ff9a65846ae3
TzunderWulf <svandenbos@tzunder.nl>
2021-10-11 21:57:03 +0200

    Added new yield section for specific links/scripts that are only used on certain pages.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,9 +6,17 @@
 
     <title>Genshin Impact Archive</title>
 
-    <!-- Bootstrap CSS + Icons -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <!-- JQuery Library -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script>
+
+    <!-- Bootstrap CSS, JS, Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    @yield('head-data')
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-light pt-3 pb-3 px-3 mb-5 shadow">
