dc9ed8b2bb5914a35b1f9ab14ec3b0b2f00fa7f0
TzunderWulf <svandenbos@tzunder.nl>
2021-10-23 19:17:50 +0200

    Added explanation about the gameplay in Genshin, and the game itself, on the 'about' page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,21 +4,57 @@
     <div class="container">
         <div class="row mx-auto mb-5 justify-content-center">
             <div class="col-md-8 border rounded border-1 p-4">
-                <div class="text-center my-3 border-bottom border-primary border-2">
-                    <h1 class="h2">The Genshin Wiki</h1>
-                    <h2 class="h5">Find all the information you need</h2>
+                <div>
+                    <div class="text-center my-3 border-bottom border-primary border-2">
+                        <h1 class="h2">The Genshin Wiki</h1>
+                        <h2 class="h5">Find all the information you need</h2>
+                    </div>
+                    <p>
+                        This archive contains the characters within the game and shows all the essential information
+                        per character.
+                    </p>
+                    <p>
+                        Vivamus vel viverra est. Aliquam et dapibus sapien, malesuada viverra sapien. Proin ornare
+                        massa ut ex placerat, eget fermentum ante suscipit. Pellentesque tempor nisi odio.
+                    </p>
                 </div>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet iaculis dignissim.
-                    Nam eget luctus velit. Quisque id venenatis turpis, vitae laoreet ipsum. Nulla sed tortor odio.
-                    Sed dapibus pulvinar lacus eu bibendum. Maecenas tincidunt justo tempus, vehicula quam et,
-                    bibendum erat. Curabitur feugiat libero sit amet condimentum vehicula.
-                </p>
-                <p>
-                    Vivamus vel viverra est. Aliquam et dapibus sapien, malesuada viverra sapien. Proin ornare
-                    massa ut ex placerat, eget fermentum ante suscipit. Pellentesque tempor nisi odio.
-                </p>
+                <div>
+                    <div class="border-bottom border-primary border-2 my-3">
+                        <h1 class="h2 text-center">About Genshin Impact</h1>
+                    </div>
+                    <p>
+                        Genshin Impact is a free-to-play action RPG developed and published by miHoYo. The game
+                        features a fantasy open-world environment and action based combat system using elemental
+                        magic, character switching, and gacha monetization system for players to obtain new characters,
+                        weapons, and other resources. The game can only be played with an internet connection and
+                        features a limited multiplayer mode allowing up to four players in a world.
+                    </p>
+                    <div class="border-bottom border-primary border-2 my-3">
+                        <h1 class="h2 text-center">Gameplay</h1>
+                    </div>
+                    <p>
+                        Players can explore the game's large fantasy world, called Teyvat, with over 35+ characters
+                        and various points of interest spread across the map. The game features an action combat
+                        playstyle that allows the player to swap between 4 individual characters in a party to quickly
+                        combo attacks. Single-player gameplay is the focus of the game, with Co-Op Mode available for
+                        certain activities.
+
+                        Each character has two unique combat skills: an Elemental Skill and an Elemental Burst. The
+                        elemental skill can be used at any time except for the cooldown period immediately after use,
+                        while the elemental burst has an energy cost, requiring the user to amass sufficient energy
+                        first.
+
+                        Characters have control over one of seven natural elements: Cryo, Dendro, Pyro, Hydro, Anemo,
+                        Electro, and Geo. Cooking is another significant aspect of Genshin Impact's gameplay. Players
+                        gather many resources in the course of their adventure, some of which can be turned into dishes
+                        via cooking. Some dishes regenerate characters' health, while others boost attack or defensive
+                        abilities.
+                    </p>
+                </div>
             </div>
+
+
+
         </div>
     </div>
 @endsection
