c44fb238f40c63d8fc060bdb519fa5890c35a787
TzunderWulf <svandenbos@tzunder.nl>
2021-10-29 19:32:07 +0200

    Several columns are now able to be NULL in the database.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role')->default('0');
-            $table->rememberToken();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
